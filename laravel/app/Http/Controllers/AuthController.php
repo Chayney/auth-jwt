781e552657034c6447e5ae0aecdcb1a0b0f51841
Chayney <chunchunmaru33333@gmail.com>
2025-11-01 09:13:59 +0900

    環境構築

--- laravel/app/Http/Controllers/AuthController.php
+++ laravel/app/Http/Controllers/AuthController.php
@@ -66,7 +66,7 @@ class AuthController extends Controller
                 'id' => $user->id,
                 'name' => $user->name,
                 'email' => $user->email,
-            ],
+            ]
         ]);
     }
 
